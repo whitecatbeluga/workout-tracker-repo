edb8729d2da68e17cf21ff1d8a649b5e765fc166
Chadie Gil <chadie.augis@jg-corporation.com>
2025-06-03 10:27:21 +0800

    update: test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:workout_tracker_repo/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const WorkoutTracker());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
