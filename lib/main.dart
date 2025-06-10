5e528bec80962fdc46ecd4775bd468efe3e423ab
ludiBala <ludivico.balatero@jg-corporation>
2025-06-10 15:29:19 +0800

    Profile Pages: - Statistics Page - Calendar Page

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:workout_tracker_repo/core/providers/user_info_provider.dart';
 import 'package:workout_tracker_repo/routes/route_generator.dart';
 import 'config/firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:workout_tracker_repo/routes/profile/profile.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +22,8 @@ class WorkoutTracker extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Workout Tracker",
-      initialRoute: '/',
+      // initialRoute: '/',
+      initialRoute: ProfileRoutes.calendar,
       onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
