68a233e18c2dad354e9f272b39a1d0b5d67be4a3
ludiBala <ludivico.balatero@jg-corporation>
2025-06-11 17:38:11 +0800

    Calendar Enhancement

---
+++ lib/presentation/domain/entities/calendar_workoutdata.dart
@@ -0,0 +1,34 @@
+class CalendarWorkoutDates {
+  final DateTime date;
+  List<UserWorkout> workouts;
+  List<String> images;
+
+  CalendarWorkoutDates({
+    required this.date,
+    required this.workouts,
+    required this.images,
+  });
+}
+
+class UserWorkout {
+  final String id;
+  final String title;
+  final String duration;
+  final String volume;
+  final String sets;
+  final String createdAt;
+
+  UserWorkout({
+    required this.id,
+    required this.title,
+    required this.duration,
+    required this.volume,
+    required this.sets,
+    required this.createdAt,
+  });
+
+  @override
+  String toString() {
+    return 'UserWorkout(id: $id, title: $title, duration: $duration, volume: $volume, sets: $sets)';
+  }
+}
