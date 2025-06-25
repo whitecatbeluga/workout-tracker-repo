1a26450917a2a98d0d1899f2336b047afe9640e9
ludiBala <ludivico.balatero@jg-corporation>
2025-06-25 08:42:39 +0800

    Measurement Image Cards

---
+++ lib/presentation/widgets/card/measurement_image_card.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class MeasurementImageCard extends StatelessWidget {
+  final DateTime date;
+  final String imageUrl;
+  const MeasurementImageCard({
+    super.key,
+    required this.date,
+    required this.imageUrl,
+  });
+
+  String _getMonthName(int month) {
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    return months[month - 1];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        imageUrl == ''
+            ? Container(
+                height: 150,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.grey,
+                ),
+                child: const Icon(
+                  Icons.fitness_center,
+                  color: Color(0xFF505050),
+                  size: 40,
+                ),
+              )
+            : Container(
+                height: 150,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: NetworkImage(imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+        Container(
+          width: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(12),
+              bottomRight: Radius.circular(12),
+            ),
+            color: const Color.fromARGB(131, 0, 0, 0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                "${date.day.toString()} ${_getMonthName(date.month)} ${date.year.toString()}",
+                style: TextStyle(color: Colors.white, fontSize: 14),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
