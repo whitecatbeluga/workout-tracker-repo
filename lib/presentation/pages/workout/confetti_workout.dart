a133536435cc4f8be51221d7b60b4f1285cacd76
kersheyheredia <kersheyheredia20@gmail.com>
2025-07-04 01:06:17 -0700

    dynamic workout count

--- lib/presentation/pages/workout/confetti_workout.dart
+++ lib/presentation/pages/workout/confetti_workout.dart
@@ -1,12 +1,15 @@
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:confetti/confetti.dart';
 import 'package:workout_tracker_repo/presentation/layouts/container.dart';
+import '../../../core/providers/auth_service_provider.dart';
 import '../../widgets/buttons/button.dart';
 
 class ConfettiWorkout extends StatefulWidget {
   const ConfettiWorkout({super.key});
+
 
   @override
   State<ConfettiWorkout> createState() => _ConfettiWorkoutState();
@@ -14,17 +17,55 @@ class ConfettiWorkout extends StatefulWidget {
 
 class _ConfettiWorkoutState extends State<ConfettiWorkout> {
   late ConfettiController _confettiController;
+  int workoutCount = 0;
+  bool isLoading = true;
 
   void initState() {
     super.initState();
     _confettiController = ConfettiController(duration: const Duration(seconds: 3));
     _confettiController.play();
+    fetchWorkoutCount();
   }
 
   @override
   void dispose() {
     _confettiController.dispose();
     super.dispose();
+  }
+  Future<void> fetchWorkoutCount() async {
+    final user = authService.value.getCurrentUser();
+
+    if (user == null) {
+      print("No user logged in.");
+      return;
+    };
+
+    print("Fetching user: ${user.uid}");
+
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('workouts')
+        .where('user_id', isEqualTo: user.uid)
+        .get();
+
+    setState(() {
+      workoutCount = querySnapshot.docs.length;
+      isLoading = false;
+    });
+
+  }
+
+  String getOrdinalSuffix(int n) {
+    if (n >= 11 && n <= 13) return '${n}th';
+    switch (n % 10) {
+      case 1:
+        return '${n}st';
+      case 2:
+        return '${n}nd';
+      case 3:
+        return '${n}rd';
+      default:
+        return '${n}th';
+    }
   }
 
   @override
@@ -50,7 +91,6 @@ class _ConfettiWorkoutState extends State<ConfettiWorkout> {
                 padding: const EdgeInsets.only(top: 100),
                 child: Column(
                   children: [
-
                     Text(
                       "Good Job!",
                       style: TextStyle(
@@ -59,7 +99,7 @@ class _ConfettiWorkoutState extends State<ConfettiWorkout> {
                       ),
                     ),
                     SizedBox(height: 10),
-                    Text("This is your 1st workout!",
+                    Text("This is your ${getOrdinalSuffix(workoutCount)} workout!",
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                     SizedBox(height: 550),
